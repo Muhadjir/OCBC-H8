bb73075acd2e221956ac322c07d61e068890e839
Muhadjir <mmtkj0206@gmail.com>
2021-10-18 10:58:25 +0700

    Sesi 10 Done

---
+++ Sesi 10/TodoApp/Configuration/AuthResult.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+namespace TodoApp.Configuration
+{
+    public class AuthResult
+    {
+        public string token {get; set;}
+        public bool success {get;set;}
+        public List<string> errors {get; set;}
+    }
+}
